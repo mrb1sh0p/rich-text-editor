d61ce8535a957e8f81b620f7195b9015d1c60ef0
João Gustavo Soares Bispo <jgbispo20@gmail.com>
2025-02-14 13:41:46 -0300

    Add basic toolbar with bold/italic/underline

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -4,6 +4,12 @@ import "./Editor.css";
 export default function Editor() {
   return (
     <div className="editor-container">
+      <div className="toolbar">
+        <button onClick={ () => document.execCommand("bold") }>B</button>
+        <button onClick={ () => document.execCommand("italic") }>I</button>
+        <button onClick={ () => document.execCommand("underline")}>U</button>
+      </div>
+
       <div
         className="editor-content"
         contentEditable

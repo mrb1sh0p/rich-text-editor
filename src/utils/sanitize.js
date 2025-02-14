1b58da2dd2e51845d098b02d23686b718d9b5693
João Gustavo Soares Bispo <jgbispo20@gmail.com>
2025-02-14 20:26:50 -0300

    Enhance sanitization with strict allow list and trusted types

--- src/utils/sanitize.js
+++ src/utils/sanitize.js
@@ -1,33 +1,27 @@
 import DOMPurify from "dompurify";
 
-export const sanitizeHTML = (html) => {
-  return DOMPurify.sanitize(html, {
-    ALLOWED_TAGS: [
-      "h1",
-      "h2",
-      "h3",
-      "h4",
-      "h5",
-      "h6",
-      "p",
-      "ul",
-      "ol",
-      "li",
-      "a",
-      "img",
-      "strong",
-      "em",
-      "u",
-      "br",
-      "div",
-      "span",
-      "table",
-      "thead",
-      "tbody",
-      "tr",
-      "th",
-      "td",
-    ],
-    ALLOWED_ATTR: ["href", "src", "alt", "class", "scope", "role"],
+const ALLOWED_TAGS = [
+  "h1",
+  "h2",
+  "h3",
+  "p",
+  "ul",
+  "ol",
+  "li",
+  "a",
+  "img",
+  "strong",
+  "em",
+  "u",
+  "br",
+];
+const ALLOWED_ATTR = ["href", "src", "alt", "class", "data-*"];
+
+export const sanitizeHTML = (html) =>
+  DOMPurify.sanitize(html, {
+    ALLOWED_TAGS,
+    ALLOWED_ATTR,
+    FORBID_TAGS: ["style", "script"],
+    FORBID_ATTR: ["onclick", "style"],
+    RETURN_TRUSTED_TYPE: true,
   });
-};

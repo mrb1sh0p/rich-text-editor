240002c0b5cb294d10442ba2e60e60198d36760a
João Gustavo Soares Bispo <jgbispo20@gmail.com>
2025-02-18 09:53:36 -0300

    Add SearchBar component with menu toggle and styles

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,8 +1,18 @@
+import "./css/Search.css";
 import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
+import { FiMenu } from "react-icons/fi";
 
-const SearchBar: React.FC<{ onSearch: (query: string) => void }> = ({
+interface SearchProps {
+  isVisible?: boolean;
+  toggleMenu?: () => void;
+  onSearch: (query: string) => void;
+}
+
+const SearchBar: React.FC<SearchProps> = ({
   onSearch,
+  isVisible,
+  toggleMenu,
 }) => {
   const { t } = useTranslation("common");
   const [query, setQuery] = useState("");
@@ -18,6 +28,12 @@ const SearchBar: React.FC<{ onSearch: (query: string) => void }> = ({
           onSearch(e.target.value);
         }}
       />
+      <button
+        onClick={toggleMenu}
+        className="menu-toggle"
+      >
+        {isVisible ? <FiMenu /> : <FiMenu />}
+      </button>
     </div>
   );
 };

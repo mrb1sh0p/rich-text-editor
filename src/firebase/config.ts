143806433d6db861927e854fe4c547c49e8266c7
João Gustavo Soares Bispo <jgbispo20@gmail.com>
2025-02-19 18:51:31 -0300

    Update Firebase configuration with new project details

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,20 +1,20 @@
-// import { getAnalytics } from "firebase/analytics";
+import { getAnalytics } from "firebase/analytics";
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: "richly-1f057.firebaseapp.com",
-  projectId: "richly-1f057",
-  storageBucket: "richly-1f057.firebasestorage.app",
-  messagingSenderId: "1066339230952",
-  appId: "1:1066339230952:web:43784dde87cc9c9b28555d",
-  measurementId: "G-Y4FDBSFSK7",
+  authDomain: "richly-38041.firebaseapp.com",
+  projectId: "richly-38041",
+  storageBucket: "richly-38041.firebasestorage.app",
+  messagingSenderId: "881751417958",
+  appId: "1:881751417958:web:0ddf0b9595fb14123ff9a7",
+  measurementId: "G-D9RGJDED76"
 };
 
 const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
+const analytics = getAnalytics(app);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
